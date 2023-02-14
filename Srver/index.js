157132d53278f218ca6537d460b4c5e2ee39d8ec
radhi75 <radhiaissa75-gmail-com@gomycode.tn>
2023-02-14 15:11:04 +0100

    iii

--- Srver/index.js
+++ Srver/index.js
@@ -34,13 +34,25 @@ nms.run();
 //   res.header("Access-Control-Allow-Origin", "http://www.abdelwahebbouden.com");
 //   next();
 // });
-app.use(
-  cors({
-    origin: "http://www.abdelwahebbouden.com",
-    credentials: false, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://www.abdelwahebbouden.com",
+//     credentials: false, //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+//   })
+// );
+const endpoint = "http://164.90.183.141/api/get/allpost";
+const corsProxy = "http://www.abdelwahebbouden.com";
+
+fetch(corsProxy + endpoint, {
+  method: "GET",
+  headers: {
+    Origin: "http://www.abdelwahebbouden.com",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
 
 
 
