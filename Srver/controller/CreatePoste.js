67fa0801344a4f1ace3d0aab312d3f458099c9d3
radhi75 <radhiaissa75-gmail-com@gomycode.tn>
2023-02-14 14:56:41 +0100

    saver rr

--- Srver/controller/CreatePoste.js
+++ Srver/controller/CreatePoste.js
@@ -1,6 +1,7 @@
 const {connection}=require('../DataBaseMySQL/config')
 module.exports={
     createPost:((req,res)=>{
+      console.log(req.body)
         const query = `INSERT INTO postes(title,content,views,likes,comments,video) VALUES("${req.body.title}","${req.body.content}","${req.body.views}","${req.body.likes}","${req.body.comments}","${req.body.video}")`;
         connection.query(query,(err,result)=>
           (err)?res.status(500).send(err):res.status(201).send('poste done')
