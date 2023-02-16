6d46c85ebde6f7f849a225b061b7d1f133139a70
radhi75 <radhiaissa75-gmail-com@gomycode.tn>
2023-02-16 12:17:04 +0100

    yiuy

--- Srver/Router/UserRouter.js
+++ Srver/Router/UserRouter.js
@@ -2,5 +2,5 @@ const router=require('express').Router();
 const PosteController=require('../controller/CreateUser');
 
 router.post('/api/Create/newuser',PosteController.CreateStudent);
-router.post('/api/login/user',PosteController.VerifyStudent)
+router.post('/api/login/user',PosteController.VerifyUser)
 module.exports={UserRouter:router}
