fb82c3c98603905aa3e7cd49f3be3f328da26046
radhi75 <radhiaissa75-gmail-com@gomycode.tn>
2023-02-16 10:40:54 +0100

    save

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const express = require("express")
 const port = 5500;
 const path = require("path");
 const cookieParser = require('cookie-parser');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 app.use(cookieParser())
 app.use(express.static(path.resolve(__dirname, './Client/dist')));
 
-app.use(
-  "/",
-  createProxyMiddleware({
-    target: "https://www.abdelwahebbouden.com",
-    changeOrigin: false,
-  })
-);
+// app.use(
+//   "/",
+//   createProxyMiddleware({
+//     target: "https://www.abdelwahebbouden.com",
+//     changeOrigin: false,
+//   })
+// );
 
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
  
   res.send(path.resolve(__dirname, './Client/dist', 'index.html'));
 });
