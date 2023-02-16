2c86d8987abf2ad43736ef26d0bc5602d9b741d7
Abidijihed <abidij55@gmail.com>
2023-02-17 00:50:34 +0100

    rr

--- Srver/controller/UpdatePost.js
+++ Srver/controller/UpdatePost.js
@@ -1,6 +1,8 @@
 const {connection}=require('../DataBaseMySQL/config')
 module.exports={
+
     UpdatePostText:((req,res)=>{
+        consle.log(req.body)
         const query=`UPDATE postestext SET bigTitle="${req.body.bigTitle}",title="${req.body.title}",content="${req.body.content}",imagees="${req.body.imagees}" WHERE id=${req.params.id}`
         connection.query(query,(err,res)=>{
             err?res.status(500).send(err):res.status(200).send('update done')
