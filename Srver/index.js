d6c6ce5fd6656afb7cb68d3faaa173655f16edf0
Abidijihed <abidij55@gmail.com>
2023-02-17 00:34:03 +0100

    rr

--- Srver/index.js
+++ Srver/index.js
@@ -3,7 +3,6 @@ const app = express();
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const cors = require("cors");
-const NodeMediaServer = require("node-media-server");
 
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ server: app });
@@ -12,21 +11,6 @@ const { PosteRouter } = require("./Router/PosteRouter");
 
 const { UserRouter } = require("./Router/UserRouter");
 
-const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60,
-  },
-  http: {
-    port: 8000,
-    allow_origin: "*",
-  },
-};
-const nms = new NodeMediaServer(config);
-nms.run();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://abdelwahebbouden.com");
   next();
@@ -43,9 +27,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-// wss.on('connection', (ws) => {
-//     ws.send('Hello, client!');
-// });
+
 
 app.use("/", PosteRouter);
 app.use("/", UserRouter);
