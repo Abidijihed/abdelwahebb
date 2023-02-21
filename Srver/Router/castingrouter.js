9c4cebe1cffef5d468a22b72ec7e67444682c73d
radhi75 <radhiaissa75-gmail-com@gomycode.tn>
2023-02-21 13:22:07 +0100

    ee

--- Srver/Router/castingrouter.js
+++ Srver/Router/castingrouter.js
@@ -1,6 +1,5 @@
 const router=require('express').Router();
 const castingcontroller=require('../controller/createcasting')
-const castingimage=require('../controller/addimagecasting')
 router.get('/api/get/casting/post',castingcontroller.GetAllPostes)
 router.post('/api/addnew/casting',castingcontroller.createcasting)
 router.put('/api/update/casting/:id',castingcontroller.updatecasting)
